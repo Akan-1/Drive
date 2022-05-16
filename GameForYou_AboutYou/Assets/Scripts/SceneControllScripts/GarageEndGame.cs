4ec3f32bccf24607f637093e352bfc79208d06eb
Eibone <70942967+Eibone@users.noreply.github.com>
2022-05-16 08:36:29 +0300

    Update

---
+++ GameForYou_AboutYou/Assets/Scripts/SceneControllScripts/GarageEndGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarageEndGame : MonoBehaviour
+{
+    public GameObject EndGamePanel;
+    public GameObject WallObj;
+
+
+    [SerializeField] public AudioSource LevelCompleteSound;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.TryGetComponent<CarMove>(out CarMove car))
+        {
+            EndGamePanel.SetActive(true);
+            LevelCompleteSound.Play();
+            WallObj.SetActive(true);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
